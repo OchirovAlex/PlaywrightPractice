9ceb2a59ac1de741cf2f09f67a04917817b810ea
OchirovAlex <aocirov098@gmail.com>
2024-05-30 21:26:07 -0700

    fixture

---
+++ tests/fixture/fixture.spec.ts
@@ -0,0 +1,7 @@
+import {expect, test} from './todo-page'
+
+
+test('fixture test', async ({page, todoPage}) => {
+    await todoPage.addTodo('spec Fixture Test')
+    await expect(page.getByTestId('todo-title')).toContainText(['spec Fixture Test'])
+})
