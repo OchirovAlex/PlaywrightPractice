f74483bad951eb239186aaec02d59140a2bd5fb6
OchirovAlex <aocirov098@gmail.com>
2024-06-13 20:49:26 -0700

    add more steps

--- tests/lambdatest/assertions.spec.ts
+++ tests/lambdatest/assertions.spec.ts
@@ -15,7 +15,7 @@ test.describe('ASSERTION', () => {
     test('assertions', async ({page})=>{
         await page.goto('https://www.lambdatest.com/');
         const title = await page.title();
-        expect.soft(title).toContain('LambdaTests');
+        expect.soft(title).toContain('LambdaTest');
         const header = await page.locator('h1')
         expect(await header.textContent()).toContain('Next-Generation')
         await expect(page.locator('img[src$="Vimeo.svg"]')).toHaveAttribute('alt', 'vimeo', {timeout: 1000});
